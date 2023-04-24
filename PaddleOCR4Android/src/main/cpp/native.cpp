4a490b9dfc1d13d73e10531c34f4fa57557613d7
equationl <equationl@qq.com>
2023-04-24 09:45:48 +0800

    修复配置 cpu_model 未生效的问题

--- PaddleOCR4Android/src/main/cpp/native.cpp
+++ PaddleOCR4Android/src/main/cpp/native.cpp
@@ -38,18 +38,20 @@ Java_com_equationl_paddleocr4android_Util_paddle_OCRPredictorNative_init(
 static paddle::lite_api::PowerMode
 str_to_cpu_mode(const std::string &cpu_mode) {
   static std::map<std::string, paddle::lite_api::PowerMode> cpu_mode_map{
-      {"LITE_POWER_HIGH", paddle::lite_api::LITE_POWER_HIGH},
-      {"LITE_POWER_LOW", paddle::lite_api::LITE_POWER_HIGH},
-      {"LITE_POWER_FULL", paddle::lite_api::LITE_POWER_FULL},
-      {"LITE_POWER_NO_BIND", paddle::lite_api::LITE_POWER_NO_BIND},
-      {"LITE_POWER_RAND_HIGH", paddle::lite_api::LITE_POWER_RAND_HIGH},
-      {"LITE_POWER_RAND_LOW", paddle::lite_api::LITE_POWER_RAND_LOW}};
-  std::string upper_key;
-  std::transform(cpu_mode.cbegin(), cpu_mode.cend(), upper_key.begin(),
-                 ::toupper);
-  auto index = cpu_mode_map.find(upper_key);
+          {"LITE_POWER_HIGH", paddle::lite_api::LITE_POWER_HIGH},
+          {"LITE_POWER_LOW", paddle::lite_api::LITE_POWER_LOW},
+          {"LITE_POWER_FULL", paddle::lite_api::LITE_POWER_FULL},
+          {"LITE_POWER_NO_BIND", paddle::lite_api::LITE_POWER_NO_BIND},
+          {"LITE_POWER_RAND_HIGH", paddle::lite_api::LITE_POWER_RAND_HIGH},
+          {"LITE_POWER_RAND_LOW", paddle::lite_api::LITE_POWER_RAND_LOW}};
+  /*std::string upper_key;
+  std::transform(cpu_mode.begin(), cpu_mode.end() + 1, upper_key.begin(),
+                 ::toupper);*/
+  // 这里原本是将传入的 cpu_mode 做了一个转换成全大写的操作，但是不知道为什么，转换后就会 find 不到这个 key，所以这里索性把转换去掉了
+  // 因此需要保证传入的 cpu_mode 是全大写的，不然依旧会查找失败
+  auto index = cpu_mode_map.find(cpu_mode);
   if (index == cpu_mode_map.end()) {
-    LOGE("cpu_mode not found %s", upper_key.c_str());
+    LOGE("cpu_mode not found %s", cpu_mode.c_str());
     return paddle::lite_api::LITE_POWER_HIGH;
   } else {
     return index->second;
